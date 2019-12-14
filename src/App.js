5b17df2132b7585ad1badf00a694af92d22a4362
Ron V <ronneyvargas@gmail.com>
2019-12-14 16:02:58 -0600

    installed tilt.js to give tilt effect to image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
 import 'tachyons';
 
@@ -7,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
-     {/* { <Logo></Logo>     
-      <ImageLinkForm></ImageLinkForm>
+      <Logo></Logo>     
+      {/* <ImageLinkForm></ImageLinkForm>
       <FaceRecognition></FaceRecognition> */}
     </div>
   );
