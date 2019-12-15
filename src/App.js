9358243a2e002172a851e4c6103bb0e297dd7092
Ron V <ronneyvargas@gmail.com>
2019-12-15 16:39:08 -0600

    added background to app.js from particles.js and added style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 import 'tachyons';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}          
+      />
       <Navigation></Navigation>
-      <Logo></Logo>     
+      <Logo></Logo>
+      <Rank></Rank>     
       <ImageLinkForm></ImageLinkForm>
       { /*<FaceRecognition></FaceRecognition> */}
     </div>
