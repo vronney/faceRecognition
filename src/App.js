3b183f6c36ead53a1f16929934d762189b080512
Ron V <ronneyvargas@gmail.com>
2019-12-28 12:03:01 -0600

    Added input event on input field and click event on button.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -19,19 +19,38 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-        params={particlesOptions}          
-      />
-      <Navigation></Navigation>
-      <Logo></Logo>
-      <Rank></Rank>     
-      <ImageLinkForm></ImageLinkForm>
-      { /*<FaceRecognition></FaceRecognition> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+ onButtonSubmit = () => {
+   console.log('click');
+ } 
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit} />
+        { /*<FaceRecognition/>S */}
+      </div>
+    );
+  }
 }
 
 export default App;
