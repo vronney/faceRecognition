4602df1b752cc0d84ea518a77073b7c24d85e21b
Ron V <ronneyvargas@gmail.com>
2019-12-30 19:19:26 -0600

    made corrections to sign in and sign out sections

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
+      isSignedIn: false,
     }
   }
 
@@ -72,6 +73,11 @@ class App extends Component {
  }
  
  onRouteChange = (route) => {
+   if (route === 'signout') {
+     this.setState({isSignedIn: false})
+   } else if (route === 'home') {
+     this.setState({isSignedIn: true})
+   }
    this.setState({route: route});
  }
 
@@ -81,7 +87,7 @@ class App extends Component {
         <Particles className='particles'
           params={particlesOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
         { this.state.route === 'home' 
           ? <div>
             <Logo />
