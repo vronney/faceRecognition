389bb4694caa414e853952f1b358582b3f545250
Ron V <ronneyvargas@gmail.com>
2020-03-15 19:22:26 -0500

    connected database to frontend

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Particles from 'react-particles-js';
 
 
 const app = new Clarifai.App({
-  apiKey: '03acc9cd5dc54d29947aa868e7a69566'
+  apiKey: 'f11c025528214a10b06bb7b89f60e0fd'
 });
 
 const particlesOptions = {
@@ -28,16 +28,34 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+    imageUrl: '',
+      box: {},
+  route: 'signin',
+    isSignedIn: false,
+      user: {
+    id: '',
+      name: '',
+        email: '',
+          entries: 0,
+            joined: ''
+  }
+}
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-    }
+    this.state = initialState;
+  }
+  
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -68,13 +86,29 @@ class App extends Component {
    app.models.predict(
      Clarifai.FACE_DETECT_MODEL, 
      this.state.input)
-     .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+     .then(response => {
+       if (response) {
+         fetch('http://localhost:3001/image', {
+           method: 'put',
+           headers: {'Content-Type': 'application/json'},
+           body: JSON.stringify({
+             id: this.state.user.id
+           })
+         })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, {entries: count}))
+          })
+          .catch(console.log);
+       }
+       this.displayFaceBox(this.calculateFaceLocation(response))
+     })
      .catch(err => console.log(err));
  }
  
  onRouteChange = (route) => {
    if (route === 'signout') {
-     this.setState({isSignedIn: false})
+     this.setState(initialState)
    } else if (route === 'home') {
      this.setState({isSignedIn: true})
    }
@@ -91,7 +125,10 @@ class App extends Component {
         { this.state.route === 'home' 
           ? <div>
             <Logo />
-            <Rank />
+            <Rank 
+            name={this.state.user.name} 
+            entries={this.state.user.entries} 
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -101,8 +138,8 @@ class App extends Component {
         :
         (
           this.state.route === 'signin'
-              ? <SignIn onRouteChange={this.onRouteChange} /> 
-              : <Register onRouteChange={this.onRouteChange} />
+              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} /> 
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         )
         }
       </div>
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,8 +8,8 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
         {'This Magic Brain will detect faces in your pictures. Give it a try.'}
       </p>
       <div className='center'>
-        <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+        <div className='form center pa4 br3 shadow-5 customForm'>
+          <input className='f4 pa2 w-70 center customFileInput' type='text' onChange={onInputChange} />
           <button 
           className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' 
           onClick={onButtonSubmit}
