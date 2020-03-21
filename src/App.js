71c2757accf625492c5875c8d1321489d207e236
Ron V <ronneyvargas@gmail.com>
2020-03-21 13:42:19 -0500

    updated fetch url

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
     .then(response => response.json())
      .then(response => {
        if (response) {
-         fetch('https://afternoon-coast-10889.herokuapp.com:3001/image', {
+         fetch('https://afternoon-coast-10889.herokuapp.com/image', {
            method: 'put',
            headers: {'Content-Type': 'application/json'},
            body: JSON.stringify({
